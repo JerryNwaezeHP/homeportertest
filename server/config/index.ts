44c3cd03f5a46051e01248d9c76cea62ee4c8cac
Jerry Nwaeze <jerry@homeporter.com>
2023-10-09 04:20:19 +0100

    Added pg and logger

--- server/config/index.ts
+++ server/config/index.ts
@@ -1,5 +1,6 @@
 import { Sequelize } from 'sequelize-typescript';
 import * as models from '../src/models';
+import { Logger } from '../src/utils/logger';
 
 export class DbContext {
     public _sequelize: Sequelize;
@@ -8,7 +9,7 @@ export class DbContext {
         const LOGGING = process.env.NODE_ENV === "development";
         this._sequelize = new Sequelize(process.env.DB_URL as string, {
             models: Object.values(models),
-            logging: LOGGING,
+            logging: LOGGING ? (s: string) => Logger.info(s) : false,
         });
     }
 
--- server/index.ts
+++ server/index.ts
@@ -2,9 +2,13 @@ import * as express from 'express'
 import * as dotenv from 'dotenv'
 import * as cors from 'cors'
 import * as http from 'http'
+import * as path from 'path'
+import * as referrerPolicy from 'referrer-policy'
+import * as cookieParser from 'cookie-parser'
 
-import createMainRouter from './src/routes'; 
+import createMainRouter from './src/routes'
 import { AccessControl, ErrorHandler } from './src/utils/middleware'
+import { Logger, HttpLogger } from './src/utils/logger'
 import { DbContext } from './config'
 
 dotenv.config()
@@ -15,51 +19,53 @@ class App {
   public app: express.Application
   public router: Record<any, any>
   public db: Record<any, any>
-  public server: http.Server;
-  public io: any;
+  public server: http.Server
+  public io: any
 
   constructor() {
     this.app = express()
     this.app.use(cors())
+    this.app.use(referrerPolicy())
+    this.app.use(cookieParser())
+    this.app.set('views', path.join(__dirname, 'views'))
+    this.app.set('view engine', 'ejs')
+
     this.config()
     this.router = express.Router()
-    this.router.use(
-      '/api/v1/',
-      createMainRouter()
-    )
-    this.server = http.createServer(this.app);
-
-    (async () => {
+    this.router.use('/api/v1/', createMainRouter())
+    this.server = http.createServer(this.app)
+    ;(async () => {
       try {
         await this.Database()
-        this.app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+        this.app.listen(PORT, () => Logger.info(`Server running on port ${PORT}`))
         const io = require('socket.io')(this.server, {
           cors: {
             origin: '*',
           },
-        });
-      
+        })
+
         io.on('connection', (socket) => {
           socket.on('message', (msg) => {
-            console.log('message: ' + msg);
-          });
-      
+            Logger.info('message: ' + msg)
+          })
+
           socket.on('session-join', (o) => {
-            socket.join(o['vhcsid']);
-            io.to(o['vhcsid']).emit('session-join-message', o.msg);
-          });
-        });
+            socket.join(o['vhcsid'])
+            io.to(o['vhcsid']).emit('session-join-message', o.msg)
+          })
+        })
       } catch (error) {
-        console.error(`Error connecting to the database:  ${error.message}`)
+        Logger.error(`Error connecting to the database:  ${error.message}`)
       }
     })()
   }
 
   private config(): void {
-    this.app.use(express.json({ limit: '20mb' }))
-    this.app.use(express.urlencoded({ limit: '20mb', extended: true }))
+    this.app.use(express.json())
+    this.app.use(express.urlencoded({ extended: true }))
     this.app.use(AccessControl)
     this.app.use(ErrorHandler)
+    this.app.use(HttpLogger)
   }
 
   private async Database() {
@@ -70,4 +76,5 @@ class App {
 const appInstance = new App()
 const db = appInstance.db
 const app = appInstance.app
+
 export default { app, db }
