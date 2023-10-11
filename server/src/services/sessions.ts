866e26a5c6b2cd68119866f0bbff8bc0ac19e3e3
Jerry Nwaeze <jerry@homeporter.com>
2023-10-11 03:04:29 +0100

    Updates on UI and deployment

--- server/src/services/sessions.ts
+++ server/src/services/sessions.ts
@@ -4,7 +4,7 @@ export default class UsersServices {
   static getSessionsPage = async (_req: Request, res: Response, callback: Function) => {
     try {
       const data = {
-        fruits: ["Apple", "Mango", "Pineapple", "Banana"]
+        fruits: ["Apple", "Mango", "Pineapple", "Banana", "Grape"]
       }
       return res.render('sessions', data); 
     } catch (e) {
